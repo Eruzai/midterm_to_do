f772efbc01457ca65a1ffdf0d0dd5dfd8cdf1415
Kevin Levasseur <kevin_levasseur@telus.net>
2024-03-26 14:16:33 -0700

    chore: add items to items table

--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -13,6 +13,20 @@ INSERT INTO items (title, user_id, category_id) VALUES ('Ghostbusters: Frozen Em
 INSERT INTO items (title, user_id, category_id) VALUES ('X-men', 3, 1);
 INSERT INTO items (title, user_id, category_id) VALUES ('Naruto', 3, 1);
 
+--TO read--
+INSERT INTO items (title, user_id, category_id) VALUES ('The Lion, the Witch, and the Wardrobe', 1, 2);
+INSERT INTO items (title, user_id, category_id) VALUES ('The Lord of the Rings: the Two Towers', 1, 2);
+INSERT INTO items (title, user_id, category_id) VALUES ('Harry Potter and the Half Blood Prince', 1, 2);
+INSERT INTO items (title, user_id, category_id) VALUES ('Dune Messiah', 1, 2);
+INSERT INTO items (title, user_id, category_id) VALUES ('Dr. Jekyll and Mr. Hyde', 1, 2);
+INSERT INTO items (title, user_id, category_id) VALUES ('Twilight', 1, 2);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Moby Dick', 2, 2);
+INSERT INTO items (title, user_id, category_id) VALUES ('The Adventures of Tom Sawyer', 2, 2);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('To Kill a Mockingbird', 3, 2);
+INSERT INTO items (title, user_id, category_id) VALUES ('Atomic Habits', 3, 2);
+
 --TO eat--
 INSERT INTO items (title, user_id, category_id) VALUES ('Lobster', 1, 3);
 INSERT INTO items (title, user_id, category_id) VALUES ('Apple Pie', 1, 3);
@@ -25,3 +39,17 @@ INSERT INTO items (title, user_id, category_id) VALUES ('French Croissant', 2, 3
 INSERT INTO items (title, user_id, category_id) VALUES ('Fish', 3, 3);
 INSERT INTO items (title, user_id, category_id) VALUES ('Avocado Toast', 3, 3);
 
+--TO buy--
+INSERT INTO items (title, user_id, category_id) VALUES ('Playing Cards', 1, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Desk Mat', 1, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Rubber Duck', 1, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Pencils', 1, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Roller Blades', 1, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Vacuum Cleaner', 1, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Digital Clock', 1, 4);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Treadmill', 2, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Mini Fridge', 2, 4);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('LED Lights', 3, 4);
+INSERT INTO items (title, user_id, category_id) VALUES ('Cat Carrier', 3, 4);
