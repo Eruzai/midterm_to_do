4b911e99c90140d3b92df30520a8ad3a9e3b2770
Lance <lanceirvingmanzanal@icloud.com>
2024-03-26 14:00:54 -0600

    chore: add data on the categories table

---
+++ db/seeds/02_categories.sql
@@ -0,0 +1,5 @@
+--Categories table seeds (EXAMPLE)--
+INSERT INTO categories (name) VALUES ('To watch')
+INSERT INTO categories (name) VALUES ('To read')
+INSERT INTO categories (name) VALUES ('To eat')
+INSERT INTO categories (name) VALUES ('To buy')
