65522f2c8dbee1288916cbeed81c545a55150cfb
Lance <lanceirvingmanzanal@icloud.com>
2024-03-26 14:16:03 -0600

    chore: add data to the items table

--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -1,2 +1,38 @@
 -- Widgets table seeds here (Example)
 
+--TO watch--
+INSERT INTO items (title, user_id, category_id) VALUES ('The Lord of the Rings: Fellowship of the Ring', 1, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('After', 1, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Godzilla x Kong: The New Empire', 1, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Dune: Part Two', 1, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('X-men', 1, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Sinister', 2, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Ghostbusters: Frozen Empire', 2, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('X-men', 3, 1);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Naruto', 3, 1);
+
+--TO eat--
+INSERT INTO items (title, user_id, category_id) VALUES ('Lobster', 1, 3);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Apple Pie', 1, 3);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Caviar', 1, 3);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Oyster', 1, 3);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Steak', 2, 3);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('French Croissant', 2, 3);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Fish', 3, 3);
+
+INSERT INTO items (title, user_id, category_id) VALUES ('Avocado Toast', 3, 3);
+
