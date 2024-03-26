89d3b0ba97e3e7a25065725f1c8602ea60f3ba18
Kevin Levasseur <kevin_levasseur@telus.net>
2024-03-26 14:20:36 -0700

    chore: linting

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (first_name, last_name, email, password) VALUES ('Alice', 'Brooks', 'AliceB@hotmail.com', 'password')
-INSERT INTO users (first_name, last_name, email, password) VALUES ('Henry', 'Jacobson', 'HenJacobs@gmail.com', 'password')
-INSERT INTO users (first_name, last_name, email, password) VALUES ('Sarina', 'Maquire', 'Sarinaisgreat@telus.net', 'password')
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Alice', 'Brooks', 'AliceB@hotmail.com', 'password');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Henry', 'Jacobson', 'HenJacobs@gmail.com', 'password');
+INSERT INTO users (first_name, last_name, email, password) VALUES ('Sarina', 'Maquire', 'Sarinaisgreat@telus.net', 'password');
