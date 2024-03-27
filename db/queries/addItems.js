e6ab5a8d13d15f67ecd9688d4af6e39b695134cf
Kevin Levasseur <kevin_levasseur@telus.net>
2024-03-27 09:22:25 -0700

    feat: insert item into items table

--- db/queries/addItems.js
+++ db/queries/addItems.js
@@ -1,1 +1,18 @@
-// SQL statement for inserting the items that the user type in the submission form into the items table
+const db = require('../connection');
+
+const addItem = (userID, categoryID) => {
+  return db
+    .query(`INSERT INTO items (title, user_id, category_id) VALUES ($1, $2, $3) RETURNING *;`, [item, userID, categoryID])
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+module.exports = { addItem };
