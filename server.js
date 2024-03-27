159d969f868761de30120079bd966ab2a4de3982
Lance <lanceirvingmanzanal@icloud.com>
2024-03-26 21:28:42 -0600

    chore: add the routes for items and categories api

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(express.static('public'));
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const itemApiRoutes = require('./routes/items-api');
+const categoriesApiRoutes = require('./routes/categories-api');
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -38,6 +40,8 @@ const usersRoutes = require('./routes/users');
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
 app.use('/users', usersRoutes);
+app.use('/api/items', itemApiRoutes);
+app.use('/api/categories', categoriesApiRoutes);
 // Note: mount other resources here, using the same pattern above
 
 // Home page
