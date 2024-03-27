a45b080735c3b52b403fd47892671aa69faf46d5
Kevin Levasseur <kevin_levasseur@telus.net>
2024-03-27 14:40:47 -0700

    feat: accept form data for item to insert into items table

--- routes/add_item.js
+++ routes/add_item.js
@@ -3,12 +3,12 @@ const router  = express.Router();
 const addItem = require('../db/queries/addItems');
 const usersItemsByList = require('../db/queries/category_list');
 
-const item = 'How to Train Your Dragon';
-const user = 1;
-const category = 1;
+router.post('/', (req, res) => {
+  const { title } = req.body;
+  const user = 1;
+  const category = 1;
 
-router.get('/', (req, res) => {
-  addItem.addItem(item, user, category)
+  addItem.addItem(title, user, category)
     .then(data => {
       res.json({ data });
     })
@@ -18,18 +18,5 @@ router.get('/', (req, res) => {
         .json({ error: err.message });
     });
 });
-
-router.post('/', (req, res) => {
-  const { title } = req.body;
-
-  return db.query('INSERT INTO items (title) VALUES ($1)', [title])
-  .then(res => {
-    console.log(res)
-    return res.rows[0]
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-})
 
 module.exports = router;
