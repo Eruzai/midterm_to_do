207091134bfd0322937f0b02d1cd98b1879b7473
Lance <lanceirvingmanzanal@icloud.com>
2024-03-28 17:34:24 -0600

    chore: make functions to fetch the items from categories

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,18 @@
 // Client facing scripts here
+$(document).ready(function() {
+  const fetchMovies = () => {
+
+  }
+
+  const fetchBooks = () => {
+
+  }
+
+  const fetchFoods = () => {
+
+  }
+
+  const fetchProducts = () => {
+
+  }
+})
