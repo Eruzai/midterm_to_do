59c50e8e2e93940c924f81fc1e2e19c12874e17d
Lance <lanceirvingmanzanal@icloud.com>
2024-03-28 12:31:14 -0600

    chore: find restaurant API

--- routes/add_item.js
+++ routes/add_item.js
@@ -24,3 +24,20 @@ module.exports = router;
 // API suggestions
 // https://www.googleapis.com/books/v1/volumes?q=<book title>
 // https://www.omdbapi.com/?apikey=52640320&t=<movie title>
+
+// const request = require('request');
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://worldwide-restaurants.p.rapidapi.com/currencies',
+//   headers: {
+//     'X-RapidAPI-Key': 'bfd2ae10dcmsh0782860466907c5p113f17jsn4f8868665966',
+//     'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
+//   }
+// };
+
+// request(options, function (error, response, body) {
+// 	if (error) throw new Error(error);
+
+// 	console.log(body);
+// });
