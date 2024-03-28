aba8b7f4e625db1cbb4b48fbdd841ca3121f4acc
Lance <lanceirvingmanzanal@icloud.com>
2024-03-27 21:42:45 -0600

    chore: refactor the post route use cookie-session to identify each user

--- routes/login.js
+++ routes/login.js
@@ -2,12 +2,23 @@ const express = require('express');
 const router  = express.Router();
 const loggedInUser = require('../db/queries/get_logged_in_user')
 
+router.get('/', (req, res) => {
+  const userID = req.body.userId;
+
+  if (userID) {
+    res.render('/')
+  }
+
+  res.render('login')
+})
+
 // Log a user in
-router.post("/", (req, res) => {
-  const userID = 1;
-
-  req.session.userId = userID;
-  res.send(loggedInUser(userID));
-});
-
+router.post('/', (req, res) => {
+  loggedInUser.getLoggedInUser(req.body.userId)
+  .then(data => {
+    const userID = data[0]
+    req.session.userId = userID
+    res.redirect('/')
+  })
+})
 module.exports = router;
