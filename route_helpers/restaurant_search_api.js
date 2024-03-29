4bbca8f6092029a54ffcb3698d4d3f208978a0c3
Lance <lanceirvingmanzanal@icloud.com>
2024-03-29 17:11:11 -0600

    fix: search for restaurant implemented

--- route_helpers/restaurant_search_api.js
+++ route_helpers/restaurant_search_api.js
@@ -3,10 +3,19 @@ const convert = require('./convert_spaces');
 
 // Looks up string to see if it exactly matches the returned restaurant title
 const findIfRestaurantExists = (string) => {
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer Ylv_u1fLkFegCR7Z-ATQ0fMh0H0DUsT49JH3fYG5993Hfc1kRIDTgv9S0g96bQoUOHIhqvt7SoBhhhVjGRu695QZtZBlcHKiwdm7CTNlu_AndQRowEhck3yFLBgHZnYx'
+    }
+  };
+
   const restaurant = convert(string)
-  return request(``)
+  return request(`https://api.yelp.com/v3/businesses/search?location=Canada&term=${restaurant}&sort_by=best_match&limit=20`, options)
   .then((body) => {
-    const data = JSON.parse(body).Title;
+    const data = JSON.parse(body).businesses[0].name;
     if(data === string) {
       return true;
     } else {
