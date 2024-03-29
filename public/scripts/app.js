93430a42e042809b0120476772b78172315b3dfc
Lance <lanceirvingmanzanal@icloud.com>
2024-03-28 23:19:28 -0600

    chore: make helper functions to fetch and display items for each category

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,34 +1,73 @@
 // Client facing scripts here
 $(document).ready(function() {
 
-  const createItemElement = (titles) => {
-    for (const title of titles) {
-      const $item = `
-        <h3>${title}</h3>
-      `
-      return $item;
-    }
+  const displayItems = (titles) => {
+    const titleList = $('.items-container');
+    titleList.empty();
+
+    const ul = $('<ul></ul>');
+
+    $.each(titles, (index, title) => {
+      const li = $('<li></li>').text(title);
+      ul.append(li);
+  });
+
+  titleList.append(ul);
   }
 
   const fetchMovies = () => {
-    
+    $.ajax({
+      url: '/api/categoryitems?categoryId=1',
+      method: 'GET',
+      success: (res) => {
+        displayItems(res.titles);
+      }
+    })
   }
 
   const fetchBooks = () => {
-
+    $.ajax({
+      url: '/api/categoryitems?categoryId=2',
+      method: 'GET',
+      success: (res) => {
+        displayItems(res.titles);
+      }
+    })
   }
 
   const fetchFoods = () => {
-
+    $.ajax({
+      url: '/api/categoryitems?categoryId=3',
+      method: 'GET',
+      success: (res) => {
+        displayItems(res.titles);
+      }
+    })
   }
 
   const fetchProducts = () => {
-
+    $.ajax({
+      url: '/api/categoryitems?categoryId=4',
+      method: 'GET',
+      success: (res) => {
+        displayItems(res.titles);
+      }
+    })
   }
 
-  const loadItems = () => {
+  $('.fetch-movies').on("click", () => {
+    fetchMovies();
+  })
 
-  }
+  $('.fetch-books').on("click", () => {
+    fetchBooks();
+  })
 
-  loadTweets();
+  $('.fetch-foods').on("click", () => {
+    fetchFoods();
+  })
+
+  $('.fetch-products').on("click", () => {
+    fetchProducts();
+  })
 })
