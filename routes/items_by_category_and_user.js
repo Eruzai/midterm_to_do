8c387f3e96180aea68d78ec0d6376ba28d385ab9
Lance <lanceirvingmanzanal@icloud.com>
2024-04-01 23:50:34 -0600

    chore: check if user is currently logged in

--- routes/items_by_category_and_user.js
+++ routes/items_by_category_and_user.js
@@ -7,6 +7,9 @@ router.get('/', (req, res) => {
   const user = req.session.userId
   const category = req.query.categoryId
 
+  if (!user) {
+   console.log("yo! u need to log in !!!")
+  }
   usersItemsByList.getCategoryList(user, category)
     .then(data => {
       const titles = data.map(item => item.title)
